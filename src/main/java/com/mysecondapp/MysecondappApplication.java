2c280d509cbf44d211182320c84398b315fc072a
harishvarma546@gmail.com <harishvarma546@gmail.com>
2020-07-02 12:27:48 +0530

    my first commit

---
+++ src/main/java/com/mysecondapp/MysecondappApplication.java
@@ -0,0 +1,13 @@
+package com.mysecondapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysecondappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysecondappApplication.class, args);
+	}
+
+}
